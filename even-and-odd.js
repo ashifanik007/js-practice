2bdc211baf6bef89fa0ba9edf4ac8f8958d001b2
ashifanik007 <mohammadashif.anik@gmail.com>
2022-02-01 07:03:30 +0600

    first commit

---
+++ even-and-odd.js
@@ -0,0 +1,24 @@
+// function getEven(num){
+//     if(num % 2 == 0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+
+// const myNumber = 5452;
+// const result = getEven(myNumber);
+// console.log(result);
+
+// function getOdd(num){
+//     if(num % 2 != 0){
+//         return true;
+//     }
+//     else {
+//         return false;
+//     }
+// }
+// const myNumber = 2222222222222221;
+// const result = getOdd(myNumber);
+// console.log(result);
